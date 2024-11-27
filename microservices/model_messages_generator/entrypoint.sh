ac6e610a5351f4e7b18eba23cd97555701c3c3c1
ascii-63 <duy.nq203690@sis.hust.edu.vn>
2024-11-27 19:21:28 +0700

    model msg gen docker

---
+++ microservices/model_messages_generator/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+    python main.py
+    exit_code=$?
+    if [ $exit_code -eq 1 ]; then
+        echo "Script exited with code 1, rerunning after 1 second..."
+        sleep 1
+    else
+        break
+    fi
+done
